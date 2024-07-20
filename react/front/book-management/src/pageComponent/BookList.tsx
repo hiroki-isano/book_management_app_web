5ffa490eb2082085c252ac71f6b60dc2f885d855
isano <isano@voyager.co.jp>
2024-07-20 14:18:30 +0900

    save

--- react/front/book-management/src/pageComponent/BookList.tsx
+++ react/front/book-management/src/pageComponent/BookList.tsx
@@ -11,6 +11,15 @@ interface Book {
   // createdAt: Date;
   // updatedAt: Date;
 }
+// {
+//   "id": 14,
+//   "uuid": "693ebb3d-8fdf-4e76-9ed1-1a31d9733e91",
+//   "title": "fdsfds",
+//   "description": "sdffsdfs",
+//   "file_path": "/book/スクリーンショット 2024-07-13 19.51.13.png",
+//   "created_at": "2024-07-15T06:06:49.575Z",
+//   "updated_at": "2024-07-15T06:06:49.575Z"
+// },
 function BookList() {
   const [books, setBooks] = useState<Book[]>([]);
 
@@ -35,6 +44,7 @@ function BookList() {
             <th className="w-1/2 px-4 py-2">id</th>
             <th className="w-1/4 px-4 py-2">Title</th>
             <th className="w-1/4 px-4 py-2">Author</th>
+            <th className="w-1/4 px-4 py-2">URL</th>
           </tr>
         </thead>
         <tbody>
@@ -43,6 +53,8 @@ function BookList() {
               <td className="border px-4 py-2">{book.id}</td>
               <td className="border px-4 py-2">{book.title}</td>
               <td className="border px-4 py-2">{book.author}</td>
+              <td className="border px-4 py-2"><a href=
+              {"http://192.168.0.120:5173/download/"+book.id}>link</a></td>
             </tr>))}
         </tbody>
       </table>
